c50327dd829c3087846a506764602be75fd49e36
Goran Tenic <tenic.g@gmail.com>
2022-04-27 17:46:35 +0200

    add superjson

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { withTRPC } from "@trpc/next";
+import { AppType } from "next/dist/shared/lib/utils";
+import superjson from "superjson";
+import { AppRouter } from "./api/trpc/[trpc]";
 
-export default MyApp
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : `http://localhost:3000/api/trpc`;
+
+    return {
+      url,
+      //   queryClientConfig: {
+      //     defaultOptions: { queries: { staleTime: 60 } },
+      //   },
+      transformer: superjson,
+    };
+  },
+  ssr: false,
+})(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from "next";
+import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(["hello"]);
+
   return (
     <main>
       <h1>Hello World</h1>
+      {!isLoading && data && <p>{data.greeting}</p>}
     </main>
   );
 };
